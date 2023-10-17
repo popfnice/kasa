734bd52353c6f379251132c49e01a9685ed5fa37
popfnice <popfnice@gmail.com>
2023-10-17 14:39:00 +0200

    mise a jour2

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,32 +1,36 @@
-import React, { useState, useRef } from "react"
-import arrow_up from "../assets/arrow_up.png"
-import arrow_down from "../assets/arrow_down.png"
-import './collapse.scss'
+import React, { useState, useRef } from "react";
+import arrow_down from "../assets/arrow_down.png";
+import './collapse.scss';
 
 export default function Collapse({ title, content }) {
-    const [isOpen, setIsOpen] = useState(false)
-    const contentRef = useRef(null)
+    const [isOpen, setIsOpen] = useState(false);
+    const contentRef = useRef(null);
 
     const handleToggleCollapse = () => {
-        setIsOpen(!isOpen)
-    }
+        setIsOpen(!isOpen);
+    };
 
     return (
         <div>
             <div
                 onClick={handleToggleCollapse}
-                className={`collapse ${isOpen ? "open" : ""}`}>
+                className={`collapse ${isOpen ? "open" : ""}`}
+            >
                 <p>{title}</p>
-                <img src={isOpen ? arrow_down : arrow_up}
-                    alt={isOpen ? "fermer" : "ouvrir"} />
+                <img
+                    className="collapse-icon"
+                    src={isOpen ? arrow_down : arrow_down}
+                    alt={isOpen ? "fermer" : "ouvrir"}
+                />
             </div>
-        {isOpen && (
-            <div
-                ref={contentRef}
-                className={`collapsible-content ${isOpen ? "open" : ""}`}>
+            {isOpen && (
+                <div
+                    ref={contentRef}
+                    className={`collapsible-content ${isOpen ? "open" : ""}`}
+                >
                     <ul>{content}</ul>
-            </div>
-        )}
+                </div>
+            )}
         </div>
-    )
+    );
 }
--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,33 +1,46 @@
-import React from 'react';
+// about.jsx
+import React, { useState } from 'react';
 import './about.scss';
 import BannerWithoutH1 from '../components/BannerWithoutH1.jsx';
 import Collapse from '../components/Collapse';
 
-
 function About() {
-  return (
-    <div className='about-description'>
-      <BannerWithoutH1 />
-      <div className="aboutCollapse">
-        <Collapse 
-          title="Fiabilité"
-          content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes"
-        />
-        <Collapse
-          title="Respect"
-          content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme"
-        />
-        <Collapse
-          title="Service"
-          content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question"
-        />
-        <Collapse
-          title="Sécurité"
-          content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
-        />
-      </div>
-    </div>
-  );
+    const [isOpenFiabilite, setIsOpenFiabilite] = useState(false);
+    const [isOpenRespect, setIsOpenRespect] = useState(false);
+    const [isOpenService, setIsOpenService] = useState(false);
+    const [isOpenSecurite, setIsOpenSecurite] = useState(false);
+
+    return (
+        <div className='about-description'>
+            <BannerWithoutH1 />
+            <div className="aboutCollapse">
+                <Collapse
+                    title="Fiabilité"
+                    content="Les annonces postées sur Kasa garantissent une fiabilité totale. Les photos sont conformes aux logements, et toutes les informations sont régulièrement vérifiées par nos équipes"
+                    isOpen={isOpenFiabilite}
+                    onToggle={() => setIsOpenFiabilite(!isOpenFiabilite)}
+                />
+                <Collapse
+                    title="Respect"
+                    content="La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme"
+                    isOpen={isOpenRespect}
+                    onToggle={() => setIsOpenRespect(!isOpenRespect)}
+                />
+                <Collapse
+                    title="Service"
+                    content="Nos équipes se tiennent à votre disposition pour vous fournir une expérience parfaite. N'hésitez pas à nous contacter si vous avez la moindre question"
+                    isOpen={isOpenService}
+                    onToggle={() => setIsOpenService(!isOpenService)}
+                />
+                <Collapse
+                    title="Sécurité"
+                    content="La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que pour les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes."
+                    isOpen={isOpenSecurite}
+                    onToggle={() => setIsOpenSecurite(!isOpenSecurite)}
+                />
+            </div>
+        </div>
+    );
 }
 
 export default About;
